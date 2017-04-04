f7a421abeb2e95b80e0edd02c4b741949b46075b
Vitor Lima <Vitor Lima>
2017-04-03 23:22:02 -0300

    My first project using Xamarin Environment

---
+++ EjercicioBase/EnviarCorreo.Forms/EnviarCorreo.Forms/ViewModel/ViewModelBase.cs
@@ -0,0 +1,42 @@
+﻿using EnviarCorreo.Forms.Classes;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EnviarCorreo.Forms.ViewModel
+{
+    public class ViewModelBase : VMBase
+    {
+        public ICommand IdentificadorCommand { get; set; }
+
+        public ViewModelBase(INavigation navigation)
+        {
+            IdentificadorCommand = new Command(async() =>
+            {
+                //Este é o lugar onde você colocar o seu endereço de e-mail
+                string direccionCorreo = "vitor.lima@msn.com";
+                string evento = "Acompanhando a Maratona da cidade de São José dos Campos/SP";
+
+                ServiceHelper servicioApp = new ServiceHelper();
+                await servicioApp.InsertarEntidad(direccionCorreo, evento);
+                BtnText = "Teste enviado";
+            });
+
+            BtnText = "Enviar teste";
+
+        }
+
+        private string _btnText;
+
+        public string BtnText
+        {
+            get { return _btnText; }
+            set
+            {
+                _btnText = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+    }
+}
---
+++ EjercicioBase/EnviarCorreo.Nativo/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using EnviarCorreo.Forms.Classes;
+
+namespace EnviarCorreo.Nativo
+{
+    [Activity(Label = "EnviarCorreo.Nativo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button button;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            button = FindViewById<Button>(Resource.Id.btnReportar);
+            button.Click += btnReportar_Click;
+        }
+
+        private async void btnReportar_Click(object sender, EventArgs e)
+        {
+            ServiceHelper serviceHelper = new ServiceHelper();
+            await serviceHelper.InsertarEntidad("vitor.lima@msn.com", "Acompanhando a Maratona da cidade de São José dos Campos/SP");
+            button.Text = "Teste enviado";
+        }
+    }
+}
+
